f3efc2a770129b7a9f5dd6c2254f24f36ca029d1
StephanieSoldavini <ss1120@rit.edu>
2017-02-12 08:40:53 +0000

    Added sessions unique to amazon account.

--- zork.py
+++ zork.py
@@ -29,24 +29,21 @@ def make_sure_path_exists(path):
 
 @ask.launch
 def new_game():
-    #uid = session.user.userID
-    #make_sure_path_exists(SESSION_BASE_PATH + uid)
-    s = subprocess.Popen([ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
+    uid = session.user.userId
+    make_sure_path_exists(SESSION_BASE_PATH + uid)
+    s = subprocess.Popen([ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True, cwd=SESSION_BASE_PATH+uid)
     out, err = s.communicate(input="restore, look", timeout=5)
     #result = list(out.decode("utf-8"))[5:-1]
     result = '\n'.join( out.split('\n')[5:-1] )
-    # s.stdout.readline()
-    return question("Welcome to Zork!\n\n" + result)
+    return question("Welcome to West of Echo!\n" + result)
 
 
 @ask.intent("ActionIntent")
 def action(action):
-    s = subprocess.Popen([ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
-    print("##########", LOAD_CMD + action + SAVE_CMD)
+    uid = session.user.userId
+    s = subprocess.Popen([ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True, cwd=SESSION_BASE_PATH+uid)
     out, err = s.communicate(input=LOAD_CMD + action + SAVE_CMD, timeout=5)
     result = '\n'.join( out.split('\n')[5:-2] )[1:]
-    print("#########2", result)
-    print("#########3", '\n'.join( out.split('\n')))
     return question(result)
 
 
