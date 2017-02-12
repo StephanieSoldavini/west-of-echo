1380b7c33e60d214401939e91fa3d8fd52ba96c4
StephanieSoldavini <ss1120@rit.edu>
2017-02-12 09:30:49 +0000

    Quit, restore, save work

--- zork.py
+++ zork.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_ask import Ask, statement, question, session
 from pprint import pprint as pp
 import os
+from shutil import copyfile
 import errno
 import subprocess
 
@@ -13,6 +14,8 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 SESSION_BASE_PATH = "/home/ubuntu/zork_session/"
+SYS_SAVE_FILENAME = "/dsave.dat"
+USR_SAVE_FILENAME = "/usr_dsave.dat"
 
 LOAD_CMD = "restore \n "
 SAVE_CMD = "\n save"
@@ -41,10 +44,25 @@ def new_game():
 @ask.intent("ActionIntent")
 def action(action):
     uid = session.user.userId
-    s = subprocess.Popen([ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True, cwd=SESSION_BASE_PATH+uid)
-    out, err = s.communicate(input=LOAD_CMD + action + SAVE_CMD, timeout=5)
-    result = '\n'.join( out.split('\n')[5:-2] )[1:]
+    if action.strip().lower() == "save":
+        """ cp dsave.dat user_dsave.dat """
+        copyfile(SESSION_BASE_PATH + uid + SYS_SAVE_FILENAME, SESSION_BASE_PATH + uid + USR_SAVE_FILENAME)
+        result = "Saved."
+    elif action.strip().lower() == "restore":
+        """ cp user_dsave.dat dsave.dat """
+        copyfile(SESSION_BASE_PATH + uid + USR_SAVE_FILENAME, SESSION_BASE_PATH + uid + SYS_SAVE_FILENAME)
+        result = "Restored."
+    else:
+        s = subprocess.Popen([ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True, cwd=SESSION_BASE_PATH+uid)
+        out, err = s.communicate(input=LOAD_CMD + action + SAVE_CMD, timeout=5)
+        result = '\n'.join( out.split('\n')[5:-2] )[1:]
     return question(result)
+
+@ask.session_ended
+def session_ended():
+    uid = session.user.userId
+    os.remove(SESSION_BASE_PATH + uid + SYS_SAVE_FILENAME)
+    return statement("Goodbye.")
 
 
 #if __name__ == '__main__':
