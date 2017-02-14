b50d8bed708514b5f99381a95e1c152f0c633917
kdmillar <kdmillar13@gmail.com>
2017-02-14 05:37:41 +0000

    Changed zork to original DOS/Windows 95 version with frotz engine emulator. zork.py now uses a regex to parse zork data output for user.

--- zork.py
+++ zork.py
@@ -1,4 +1,5 @@
 import os
+import re
 import errno
 import logging
 import subprocess
@@ -11,14 +12,17 @@ ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 SESSION_BASE_PATH = "/home/ubuntu/zork_session/"
-SYS_SAVE_FILENAME = "/dsave.dat"
+SYS_SAVE_FILENAME = "dsave.dat"
 USR_SAVE_FILENAME = "/usr_dsave.dat"
 
-LOAD_CMD = "restore \n "
-SAVE_CMD = "\n save"
+LOAD_CMD = "restore\n"
+SAVE_CMD = "\nsave\n\ny"
+QUIT_CMD = "\nquit\ny\n"
 
-ZORK_BINARY = "/home/ubuntu/zork/zork"
+DFROTZ_BINARY = "/home/ubuntu/zork1/dfrotz"
+ZORK_BINARY = "/home/ubuntu/zork1/DATA/ZORK1.DAT"
 
+regex = r">Please enter a filename(.|\n)*> ?((?P<location>.*?)\W*Score:.*Moves:.*\n\n)?((?P=location)\n)?(?P<out>(.|\n)*)\n>Please enter a filename.*"
 
 def make_sure_path_exists(path):
     try:
@@ -28,11 +32,13 @@ def make_sure_path_exists(path):
             raise
 
 def call_zork(uid, action):
-    s = subprocess.Popen([ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True, cwd=SESSION_BASE_PATH+uid)
-    out, err = s.communicate(input=LOAD_CMD + action + SAVE_CMD, timeout=5)
-    return '\n'.join( out.split('\n')[5:-2] )[1:]
-
-
+    s = subprocess.Popen([DFROTZ_BINARY, ZORK_BINARY], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True, cwd=SESSION_BASE_PATH+uid)
+    out, err = s.communicate(input=LOAD_CMD + SYS_SAVE_FILENAME + '\n' + action + SAVE_CMD + QUIT_CMD, timeout=5)
+    print("########################")
+    print(out)
+    print("########################")
+    return re.search(regex, out).group('out')
+      
 @ask.launch
 def new_game():
     uid = session.user.userId
@@ -46,12 +52,12 @@ def action(action):
     uid = session.user.userId
     if action.strip().lower() == "save":
         """ cp dsave.dat user_dsave.dat """
-        copyfile(SESSION_BASE_PATH + uid + SYS_SAVE_FILENAME, SESSION_BASE_PATH + uid + USR_SAVE_FILENAME)
+        copyfile(SESSION_BASE_PATH + uid + '/' + SYS_SAVE_FILENAME, SESSION_BASE_PATH + uid + USR_SAVE_FILENAME)
         result = "Saved."
     elif action.strip().lower() == "restore":
         """ cp user_dsave.dat dsave.dat """
         if (os.path.isfile(SESSION_BASE_PATH + uid + USR_SAVE_FILENAME)):
-            copyfile(SESSION_BASE_PATH + uid + USR_SAVE_FILENAME, SESSION_BASE_PATH + uid + SYS_SAVE_FILENAME)
+            copyfile(SESSION_BASE_PATH + uid + USR_SAVE_FILENAME, SESSION_BASE_PATH + uid + '/' + SYS_SAVE_FILENAME)
         result = "Restored."
     else:
         result = call_zork(uid, action)
@@ -62,7 +68,7 @@ def action(action):
 def session_ended():
     if (request.reason == "USER_INITIATED"):
         uid = session.user.userId
-        os.remove(SESSION_BASE_PATH + uid + SYS_SAVE_FILENAME)
+        os.remove(SESSION_BASE_PATH + uid + '/' + SYS_SAVE_FILENAME)
     return statement("Goodbye.")
 
 
